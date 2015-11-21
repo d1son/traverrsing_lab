3d7c5b17f2e1d80d1927c510d0b8fa65e67e70b2
Sean Conaty <anxiolydiot@nbp-253-2.nbp.ruw.rutgers.edu>
2015-11-21 14:06:57 -0500

    current js

---
+++ function.js
@@ -0,0 +1,32 @@
+$("#addsButton").on("click", function() {
+    $(this).add("#addsButton"));
+})
+
+$("#addsForm").on("click", function() {
+    $(this).add("#addsForm"));
+})
+
+$("#addsDiv").on("click", function() {
+    $(this).add("#addsDiv"));
+});
+
+$("#addsButton").dblclick(function(){
+    $(this).remove("#addsDiv"));
+});
+
+var whateverObj = []
+ 
+};
+
+$.each("whateverObj").on("mouseover", function() {
+    $(this).parents().unbind("mouseleave");
+    
+
+    
+});
+
+
+
+
+    
+
